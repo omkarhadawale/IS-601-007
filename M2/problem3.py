f2fa6035f51e83a9c34c9c43aca5d2e3ddcf3f88
omkarhadawale <omkarhadawale999@gmail.com>
2022-09-23 16:27:04 -0400

    Problem 3 updated for converting to positive

--- M2/problem3.py
+++ M2/problem3.py
@@ -16,7 +16,22 @@ def process_array(num, arr):
     print(arr)
     print("\nPositive Output:\n")
     # TODO add new code here to print the desired result
-    for i in arr: print(i,end=', ') if float(i) > 0.0 else print(end='') #Printing only positive values from the list
+    """
+    IF - If the item in the list is of type string then first converting it to int or float types.Then
+    if its negative converting it to its respective positive value in int or float and finally 
+    output as string. 
+
+    ELSE - The items of type int and float will directly shown as ouput if positive else will be converted 
+    to positive and then will be shown as output
+    """
+    for i in arr: 
+        if type(i) == str:
+            if '.' in i: # If a string contains '.' then it is a float else it will be integer
+                print(i,end=', ') if float(i) > 0.0  else print((str(float(i)*-1)),end=', ') 
+            else:
+                print(i,end=', ') if int(i) > 0  else print((str(int(i)*-1)),end=', ')
+        else :    
+            print(i,end=', ') if type(i)(i) > type(i)(0)  else print((i*-1),end=', ') 
 
 
 print("Problem 3")
