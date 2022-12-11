96106ea8baf63688892b24dfd1c9615a401dbe76
omkarhadawale <omkarhadawale999@gmail.com>
2022-12-11 16:13:17 -0500

    User profile update and password update added on landing page

--- FinalProject/auth/auth.py
+++ FinalProject/auth/auth.py
@@ -1,6 +1,7 @@
+import json
 import re
 from flask import Blueprint, render_template, flash, redirect, url_for,current_app, session
-from auth.forms import LoginForm, RegisterForm
+from auth.forms import ChangeUserPassword, LoginForm, RegisterForm, UserProfileEdit
 from sql.db import DB
 
 from flask_login import login_user, login_required, logout_user
@@ -47,7 +48,7 @@ def login():
         email = form.email.data
         password = form.password.data
         try:
-            result = DB.selectOne("SELECT id, email, password FROM IS601_Users where email=%s", email)
+            result = DB.selectOne("SELECT id, username, email, password FROM IS601_Users where email=%s", email)
             if result.status and result.row:
                 hash = result.row["password"]
                 if bcrypt.check_password_hash(hash, password):
@@ -71,7 +72,7 @@ def login():
                         # store user object in session as json
                         session["user"] = user.toJson()
                         flash("Log in successful", "success")
-                        return redirect(url_for("auth.landing_page"))
+                        return redirect(url_for("auth.landing_page",))
                     else:
                         flash("Error logging in", "danger")
                 else:
@@ -89,16 +90,72 @@ def login():
                 flash(str(e), "danger")   
     return render_template("login.html", form=form)
 
-@auth.route("/landing-page", methods=["GET"])
+@auth.route("/landing-page", methods=["GET","POST"])
 @login_required
 def landing_page():
-    
-    return render_template("landing_page.html")
+    print("-----------------",session["user"])
+    print("-------------------",type(session["user"]))
+    #UCID:-oh45
+    #Accessing logged In user from session
+    user = json.loads(session["user"])
+    #UCID:-oh45
+    #Prefilling user profile edit form from session data
+    userform = UserProfileEdit(obj=User(user['id'],user['username'],user['email'],user['roles']))
+    passform = ChangeUserPassword()
+    if userform.validate_on_submit():
+        username = userform.username.data
+        email = userform.email.data
+        update = True
+        if username == user['username'] and email==user['email']:
+            update = False
+        if update:
+            try:
+                result = DB.insertOne("UPDATE IS601_Users set username = %s, email = %s WHERE email = %s", username, email, user['email'])  
+                if result.status:
+                    flash("User profile successfully updated","success")
+                    # UCID:-oh45
+                    # Updating session 
+                    newuser = {"id":user['id'],"username":username,"email":email,"roles":user['roles']}
+                    print(newuser)
+                    try:
+                        usr = User(**newuser)
+                        session["user"] = usr.toJSON()
+                    except Exception as e:
+                        flash("Their was an error updating user session","danger")
+                        flash(f"Error message:-{str(e)}","danger")    
+            except Exception as e:
+                flash("Theie was an error updating username/email","danger")
+                flash(f"Error message:- {str(e)}","danger")
+    #UCID:-oh45
+    if passform.validate_on_submit():        
+        oldpassword = passform.oldpassword.data
+        newpassword = passform.newpassword.data
+        try:
+            result = DB.selectOne('SELECT password FROM IS601_Users where email=%s',user['email'])
+            if result.status and result.row:
+                if bcrypt.check_password_hash(result.row['password'], oldpassword):
+                    try:
+                        hash = bcrypt.generate_password_hash(newpassword)
+                        result1 = DB.insertOne('UPDATE IS601_Users SET password=%s where email=%s', hash, user['email'])
+                        if result1.status:
+                            flash("Password successfully updated","success")
+                    except Exception as e:
+                        flash("Their was an error updating the user","danger")
+                        flash(f"Error message:-{str(e)}","danger")    
+                else:
+                    flash("Wrong old password (please enter correct password)","warning")        
+        except Exception as e:
+            flash("Their was an error updating the password")
+            flash(f"Error message:-{str(e)}","danger")
+
+
+
+    return render_template("landing_page.html", form=userform, form1=passform)
 
 @auth.route("/logout", methods=["GET"])
 def logout():
     logout_user()
-     # Remove session keys set by Flask-Principal
+    # Remove session keys set by Flask-Principal
     for key in ('identity.name', 'identity.auth_type'):
         session.pop(key, None)
 
--- FinalProject/auth/forms.py
+++ FinalProject/auth/forms.py
@@ -13,3 +13,14 @@ class LoginForm(FlaskForm):
     email = EmailField("email", validators=[DataRequired(), Email()])
     password = PasswordField("password", validators=[InputRequired()])
     submit = SubmitField("Login")
+
+class UserProfileEdit(FlaskForm):
+    username  = StringField("username",validators=[InputRequired()])
+    email = EmailField("email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Update")
+
+class ChangeUserPassword(FlaskForm):
+    oldpassword = PasswordField("old password", validators=[InputRequired()])
+    newpassword = PasswordField("new password", validators=[InputRequired(), EqualTo('confirmnewpassword', message='Passwords must match'),Length(min=6, max=20, message="The password should be minimum 6 characters in length")])
+    confirmnewpassword = PasswordField("confirm new password", validators=[DataRequired()])
+    submit = SubmitField("Set Password")
